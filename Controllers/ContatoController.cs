e1aaffc9e9707ebcf32cb04787d557adff99d3ff
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2020-09-02 11:27:59 -0300

    Adicionada as controllers ContatoController e EnderecoController pois estavam sendo ignoradas.

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrcamentariaBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+
+        private ContatoService ContatoService;
+
+        public ContatoController(ContatoService contatoService)
+        {
+            this.ContatoService = contatoService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ContatoModel> Get()
+        {
+            try
+            {
+                return ContatoService.Get();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("buscar/{pessoaId}/{tipoContato}")]
+        public IEnumerable<ContatoModel> Get(int pessoaId, string tipoContato)
+        {
+            try
+            {
+                return ContatoService.Get(pessoaId, tipoContato);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("buscar")]
+        public IEnumerable<ContatoModel> Get([FromQuery] ContatoQO contato)
+        {
+            try
+            {
+                return ContatoService.GetComParametro(contato);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ContatoModel Post([FromBody] ContatoModel contato)
+        {
+            try
+            {
+                return ContatoService.Post(contato);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut("{contatoId}")]
+        public void Put(int contatoId, [FromBody] ContatoModel contato)
+        {
+            try
+            {
+                ContatoService.Put(contatoId, contato);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("deletar")]
+        public void Delete([FromQuery] ContatoQO contato)
+        {
+            try
+            {
+                ContatoService.DeleteComParametro(contato);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Controllers/EnderecoController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrcamentariaBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnderecoController : ControllerBase
+    {
+        private EnderecoService EnderecoService;
+
+        public EnderecoController(EnderecoService enderecoService)
+        {
+            this.EnderecoService = enderecoService;
+        }
+
+        [HttpGet]
+        public IEnumerable<EnderecoModel> Get()
+        {
+            try
+            {
+                return EnderecoService.Get();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("buscar")]
+        public IEnumerable<EnderecoModel> Get([FromQuery] EnderecoQO endereco)
+        {
+            try
+            {
+                return EnderecoService.GetComParametro(endereco);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public EnderecoModel Post([FromBody] EnderecoModel endereco)
+        {
+            try
+            {
+                return EnderecoService.Post(endereco);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut("{enderecoId}")]
+        public void Put(int enderecoId, [FromBody] EnderecoModel endereco)
+        {
+            try
+            {
+                EnderecoService.Put(enderecoId, endereco);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("deletar")]
+        public void Delete([FromQuery] EnderecoQO endereco)
+        {
+            try
+            {
+                EnderecoService.DeleteComParametro(endereco);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
